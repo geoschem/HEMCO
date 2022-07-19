9c40c7807bbff57ef6544aa5cdb5128261dd5625
Bob Yantosca <yantosca@seas.harvard.edu>
2022-07-19 12:07:01 -0400

    Updated _static folders in conf.py; added docs/.gitignore
    Fixed the path to theme_overrides.css in geos-chem-shared-docs/_static
    
    Also added a .gitignore file to ignore emacs autosave files *.*~*
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,14 +97,16 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = [
-    'geos-chem-shared-docs/_static'
+    '_static',
+    'geos-chem-shared-docs/_static',
 ]
 
+# Override default settings in sphinx-rtd-theme
 html_context = {
     'css_files': [
-        'theme_overrides.css',  # overrides for wide tables in RTD theme
-        ],
-    }
+        '_static/theme_overrides.css',
+    ],
+}
 
 # Display GEOS-Chem logo
 html_favicon = 'geos-chem-shared-docs/_static/favicon.png'
