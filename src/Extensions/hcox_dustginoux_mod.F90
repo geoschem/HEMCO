922b3c4359fbac81d3b0992fb825f00fda0c479e
Lizzie Lundgren <elundgren@seas.harvard.edu>
2024-11-20 14:52:12 -0700

    Pass HcoState%Config%outLUN to HCO_MSG calls without passing Err object
    This specifies where to write the messages since log information in
    the Err object is not used.
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- src/Extensions/hcox_dustginoux_mod.F90
+++ src/Extensions/hcox_dustginoux_mod.F90
@@ -566,7 +566,7 @@ CONTAINS
        IF ( HCO_IsVerb( HcoState%Config%Err ) ) THEN
           CALL HCO_Msg( HcoState%Config%Err, sep1='-' ) ! with separator
        ELSE
-          CALL HCO_Msg( msg                           ) ! w/o separator
+          CALL HCO_Msg( msg, LUN=HcoState%Config%outLUN ) ! w/o separator
        ENDIF
      
        ! Write all other messages as debug printout only
