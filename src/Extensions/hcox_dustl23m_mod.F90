1d3554f3a014f68b7ad8f96e2b2c7601ea1aadd1
Dandan Zhang <dandan.z@wustl.edu>
2025-03-13 12:03:13 -0500

    update

--- src/Extensions/hcox_dustl23m_mod.F90
+++ src/Extensions/hcox_dustl23m_mod.F90
@@ -1240,11 +1240,11 @@ CONTAINS
 
         ! requires all inputs has definition, also using a small value instead of 0
         IF ((DUST_EMIS_FLUX_Tmp(I,J) > 1.0e-15_hp) .and. &
-            (f_bare > 1.0e-15_hp) .and. &
-            (Inst%f_clay > 1.0e-15_hp) .and. &
-            (Inst%bulk_den > 1.0e-15_hp) .and. &
-            (Inst%poros > 1.0e-15_hp) .and. &
-            (Inst%roughness_r > 1.0e-15_hp)) THEN
+            (f_bare(I,J) > 1.0e-15_hp) .and. &
+            (Inst%f_clay(I,J) > 1.0e-15_hp) .and. &
+            (Inst%bulk_den(I,J) > 1.0e-15_hp) .and. &
+            (Inst%poros(I,J) > 1.0e-15_hp) .and. &
+            (Inst%roughness_r(I,J) > 1.0e-15_hp)) THEN
           DUST_EMIS_FLUX(I,J) = DUST_EMIS_FLUX_Tmp (I,J)
         ENDIF
       ENDDO
